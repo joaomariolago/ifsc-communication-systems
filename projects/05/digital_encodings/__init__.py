1e8dfcfada46eda7080b5d28def2c76c6cdda432
Joao Mario Lago <joao.mario.lago@hotmail.com>
2025-06-04 08:16:18 -0300

    Add project 05

---
+++ projects/05/digital_encodings/__init__.py
@@ -0,0 +1,17 @@
+from digital_encodings.encodings import (
+    manchester_encoding,
+    ami_encoding,
+    rz_polar_encoding,
+    manchester_decoding,
+    ami_decoding,
+    rz_polar_decoding
+)
+
+__all__ = [
+    "manchester_encoding",
+    "ami_encoding",
+    "rz_polar_encoding",
+    "manchester_decoding",
+    "ami_decoding",
+    "rz_polar_decoding"
+]
